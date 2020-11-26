3666810118a7fd067b318f7117fec4fa69e33e73
EC2 Default User <ec2-user@ip-172-31-45-46.ap-northeast-1.compute.internal>
2020-11-26 14:05:04 +0000

    タイムライン昨日の実装

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,6 @@
 class TopController < ApplicationController
   def index
-    @tweets = Tweet.includes(:user).order('created_at DESC').page(params[:page]).per(5)
+    timeline = Timeline.order('created_at DESC')
+    @timelines = timeline.map(&:timelineable)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-    root 'top#index'
-    resources :tweets do
+    root to: "top#index"
+    resources :tweets, exept: [:index] do
       resources :comments, only:[:create, :destroy]
     end
     resources :users, only:[:show]
